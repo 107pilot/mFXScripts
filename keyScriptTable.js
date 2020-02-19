0a75b1268d7af6268c5c92109c3bb1f04884a35e
107pilot <107pilot@gmail.com>
2020-02-19 15:05:16 -0500

    Update keyScriptTable.js

--- keyScriptTable.js
+++ keyScriptTable.js
@@ -2,7 +2,7 @@ $(function() {
 var ul = document.getElementById("foo");
 var items = ul.getElementsByTagName("li");
 for (var i = 0; i < items.length; ++i) {
-  console.log(var[i]);
+  console.log(items);
 }
 
 $("#example-table").tabulator("setData", items);
