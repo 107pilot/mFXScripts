0bff5452cc1adc7c0fef41c8a59f839fbf2ef47c
107pilot <107pilot@gmail.com>
2020-02-19 14:51:28 -0500

    Create keyScriptTable.js

---
+++ keyScriptTable.js
@@ -0,0 +1,11 @@
+var ul = document.getElementById("foo");
+var items = ul.getElementsByTagName("li");
+for (var i = 0; i < items.length; ++i) {
+  console.log(var[i]);
+}
+
+
+
+
+
+$("#example-table").tabulator("setData", items);
